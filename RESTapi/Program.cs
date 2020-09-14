13772462862a5e72e49ba63e0a53b2247c14361d
RediGt <red.i@inbox.lv>
2020-09-14 18:32:14 +0300

    Add project files.

---
+++ RESTapi/Program.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net;
+
+namespace RESTapi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String url = "https://my-json-server.typicode.com/RediGt/FakeRest/db";
+            WebClient client = new WebClient();
+            String response = client.DownloadString(url);
+            Console.WriteLine(response);
+
+            Container rec = JsonConvert.DeserializeObject<Container>(response);
+            foreach (Students e in rec.students)
+            {
+                Console.WriteLine(e.lastName + "  " + e.age);
+            }
+
+            /*String url = "http://dummy.restapiexample.com/api/v1/employees";
+
+            WebClient client = new WebClient();
+
+            client.Headers.Add("Content-Type", "application/json");
+            client.Headers.Add("Authentification-Token", "key");
+
+            String response = client.DownloadString(url);
+
+            //Console.WriteLine(response);
+
+            Container rec = JsonConvert.DeserializeObject<Container>(response);
+
+            if (rec.status == "success")
+            {
+                foreach (Employee e in rec.data)
+                {
+                    Console.WriteLine(e.employee_name);
+                }
+            }*/
+        }
+    }
+}
---
+++ RESTapi/Students.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RESTapi
+{
+    class Students
+    {
+        public String name;
+        public String lastName;
+        public int age;
+
+
+        public Students(String name, String lastName, int age)
+        {
+            this.name = name;
+            this.lastName = lastName;
+            this.age = age;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine(name + "  " + lastName + "  " + age);
+        }
+    }
+}
